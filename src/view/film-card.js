1078aa15aaf1fe949f04c6aa29e408bda2e7efeb
Lydia Zakharova <jahontis@gmail.com>
2020-08-06 20:25:54 +0300

    добавляет флаги в картчоку фильма

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -1,5 +1,10 @@
 export const createFilmCardTemplate = (film) => {
-  const {title, rating, release, runtime, genres, poster, description, comments} = film;
+  const {title, rating, release, runtime, genres, poster, description, comments, inWatchlist, isWatched, isFavorite} = film;
+
+  const shortenedDescription = description.length > 140 ? `${description.substr(0, 139)}…` : description;
+
+  const addActiveClass = (flag) => flag ? `film-card__controls-item--active` : ``;
+
   return (
     `<article class="film-card">
       <h3 class="film-card__title">${title}</h3>
@@ -10,12 +15,12 @@ export const createFilmCardTemplate = (film) => {
         <span class="film-card__genre">${genres}</span>
       </p>
       <img src="${poster}" alt="${title}" class="film-card__poster">
-      <p class="film-card__description">${description}</p>
+      <p class="film-card__description">${shortenedDescription}</p>
       <a class="film-card__comments">${comments.length} comments</a>
       <form class="film-card__controls">
-        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-        <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist ${addActiveClass(inWatchlist)}">Add to watchlist</button>
+        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched ${addActiveClass(isWatched)}">Mark as watched</button>
+        <button class="film-card__controls-item button film-card__controls-item--favorite ${addActiveClass(isFavorite)}">Mark as favorite</button>
       </form>
     </article>`
   );
